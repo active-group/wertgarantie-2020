eb7f9bf110fc46c883bbf14fb88d836a41ae16d3
Tim Digel <tim.digel@active-group.de>
2020-09-23 14:02:24 +0200

    Update

--- intro/lib/intro.ex
+++ intro/lib/intro.ex
@@ -58,7 +58,7 @@ defmodule Intro do
 
   use QuickStruct, [key: integer(), value: String.t()]
 
-  def intro1(), do: make(1, "Hallo")
+  def intro1(), do: make("1", "Hallo")
   def intro2(), do: make(2, "blubb")
   def intro3(), do: %Intro{value: "3. Wert", key: 3}
 
