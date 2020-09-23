9c6788c06b13fd0d718469d633716ff083e341a8
Kaan Sahin <kaan.sahin@active-group.de>
2020-09-23 09:35:13 +0200

    Update

--- racket/tag3-rekursion-ueber-zahlen.rkt
+++ racket/tag3-rekursion-ueber-zahlen.rkt
@@ -1,7 +1,7 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname tag3-rekursion-ueber-zahlen) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
-;;; Eine natürliche ist eins der folgenden
+;;; Eine natürliche Zahl ist eins der folgenden
 ;; - 0
 ;; - der Nachfolger einer natürlichen Zahl (n + 1)
 ;; (Peano-Definition)
