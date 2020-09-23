e6d03ee30b61b1a76a2b9cd10ac7e3005d89125f
Tim Digel <tim.digel@active-group.de>
2020-09-23 10:53:05 +0200

    Update

--- intro/mix.exs
+++ intro/mix.exs
@@ -5,7 +5,7 @@ defmodule Intro.MixProject do
     [
       app: :intro,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
