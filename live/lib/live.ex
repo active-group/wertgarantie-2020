d59541739c96a1238f70106fd0b507fc092b82cd
Tim Digel <tim.digel@active-group.de>
2020-09-24 15:59:08 +0200

    Update

--- live/lib/live.ex
+++ live/lib/live.ex
@@ -3,3 +3,23 @@ defmodule Live do
   Documentation for `Live`.
   """
 end
+
+defmodule Live.Supervisor do
+  @moduledoc """
+  Überwacht unseren Live.SchadenCacher GenServer
+  """
+
+  use Supervisor
+
+  def start_link(state) do
+    Supervisor.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(_state) do
+    children = [
+      worker(Live.SchadenCacher, [Live.SchadenCacher.init_schaden()], restart: :permanent)
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
