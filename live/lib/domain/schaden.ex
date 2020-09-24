5a83235903c57c5f925475329617913e1de06046
Tim Digel <tim.digel@active-group.de>
2020-09-24 14:38:42 +0200

    Update

--- live/lib/domain/schaden.ex
+++ live/lib/domain/schaden.ex
@@ -1,4 +1,4 @@
-defmodule Live.Domain.Schaden
+defmodule Live.Domain.Schaden do
   @moduledoc """
   Der Datentyp Schaden besteht aus:
   - id (int)
@@ -7,11 +7,19 @@ defmodule Live.Domain.Schaden
   - partner_nr (int): Partner-Nummer der versicherten Person
   """
 
-  # TODO Strcut
+  use QuickStruct,
+    id: integer(),
+    amount: float(),
+    description: String.t(),
+    partner_nr: integer()
 
   @doc "Bekommt zwei Schäden und liefert den Schaden mit der höheren Schadenssumme zurück"
-  @spec with_max_amount(..., ...) :: ...
-  def with_max_amount(..., ...) do
-
+  @spec with_max_amount(Schaden.t(), Schaden.t()) :: Schaden.t()
+  def with_max_amount(first, second) do
+    if first.amount >= second.amount do
+      first
+    else
+      second
+    end
   end
 end
--- live/test/domain/schaden_test.exs
+++ live/test/domain/schaden_test.exs
@@ -1,5 +1,21 @@
 defmodule Live.Domain.SchadenTest do
   use ExUnit.Case
 
+  alias Live.Domain.Schaden
+
+  test "schaden with higher amount" do
+    s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+
+    s2 =
+      Schaden.make(2, 500, "Stoßstangen sind heutzutage lackiert, da wirds schnell teuer", 9999)
+
+    assert Schaden.with_max_amount(s1, s2) == s2
+  end
+
+  test "schaden with equals amount" do
+    s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+
+    assert Schaden.with_max_amount(s1, %{s1 | description: "Andere Bagatelle"}) == s1
+  end
 
 end
