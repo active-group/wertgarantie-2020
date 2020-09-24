260c7419b4234782e5fa636f8ea16e853748d146
Tim Digel <tim.digel@active-group.de>
2020-09-24 14:02:11 +0200

    Update

--- intro/lib/process_demo.ex
+++ intro/lib/process_demo.ex
@@ -32,4 +32,73 @@ defmodule ProcessDemo do
   #   n = func.()
   #   IO.puts("Ergebnis ist #{n}")
   # end
+
+  # >>> Server
+
+  defmodule Inc do
+    @moduledoc """
+    Beinhaltet die Zahl, um die erhöht werden soll
+    """
+    use QuickStruct, i: integer()
+  end
+
+  defmodule Get do
+    @moduledoc """
+    Beinhaltet die Prozess-ID desjenigen, der die Antwort erhalten soll
+    """
+    use QuickStruct, sender_pid: pid()
+  end
+
+  @doc """
+  Ein increment Server speichert eine Zahl und kann zwei Nachrichten erhalten:
+  - Inc: Erhöhe um die Zahl i
+  - Get: Gebe die Zahl aus
+
+  Der Startwert wird als Argument übergeben.
+  """
+  @spec inc_loop(integer()) :: any()
+  def inc_loop(aktueller_wert) do
+    receive do
+      %Inc{i: i} ->
+        # IO.puts(aktueller_wert + i)
+        inc_loop(aktueller_wert + i)
+
+      %Get{sender_pid: sender_pid} ->
+        send(sender_pid, aktueller_wert)
+        inc_loop(aktueller_wert)
+    end
+  end
+
+  @doc "Starte den Server"
+  @spec start_inc_loop(integer()) :: true
+  def start_inc_loop(startwert) do
+    pid = spawn(fn -> inc_loop(startwert) end)
+    # pid = spwan(ProcessDemo, :inc_loop, [startwert])
+
+    Process.register(pid, :inc_server)
+  end
+
+  # <<< Server
+
+  # >>> Client
+  @doc "Sende Inc mit i an den Server"
+  @spec inc(integer()) :: :ok
+  def inc(i) do
+    # %Inc{i: zahl}
+    send(:inc_server, Inc.make(i))
+
+    :ok
+  end
+
+  @doc "Sende Get an Server"
+  @spec get() :: integer()
+  def get() do
+    send(:inc_server, Get.make(self()))
+
+    receive do
+      msg -> msg
+    end
+  end
+
+  # <<< Client
 end
