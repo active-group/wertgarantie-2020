f23eb58d25fa7ac76f8a3ccca24adb34c7419183
Tim Digel <tim.digel@active-group.de>
2020-09-24 11:17:38 +0200

    Update

--- intro/test/intro_test.exs
+++ intro/test/intro_test.exs
@@ -88,6 +88,8 @@ defmodule IntroTest do
   test "readme is long enough? can raise" do
     refute Intro.readme_long_enough_can_raise?("README.md")
     assert Intro.readme_long_enough_can_raise?("mix.exs")
-    # refute Intro.readme_long_enough_can_raise?("mich-gibt-esnicht-als-datei.txt")
+    assert_raise File.Error, fn ->
+      Intro.readme_long_enough_can_raise?("mich-gibt-esnicht-als-datei.txt")
+    end
   end
 end
