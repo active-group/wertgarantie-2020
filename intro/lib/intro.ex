0340e2c18a41be53aea040f5c3c75262e4245091
Tim Digel <tim.digel@active-group.de>
2020-09-24 10:55:04 +0200

    Update

--- intro/lib/intro.ex
+++ intro/lib/intro.ex
@@ -179,7 +179,8 @@ defmodule Intro do
   @doc "Prüfe ob eine übergebene Readme-Datei mehr als 21 Zeilen lang ist"
   @spec readme_long_enough?(String.t(), integer()) :: boolean()
   def readme_long_enough?(file, limit \\ 21) do
-    IO.inspect(File.read(file))
+    File.read(file)
+    |> IO.inspect
 
   end
 end
