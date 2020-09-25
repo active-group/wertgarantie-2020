7b3c1d5c353bde88a3e94912bb1504ca9c47a5ae
Tim Digel <tim.digel@active-group.de>
2020-09-25 11:26:30 +0200

    Update

--- live/mix.exs
+++ live/mix.exs
@@ -4,7 +4,7 @@ defmodule Live.MixProject do
   def project do
     [
       app: :live,
-      version: "0.1.1",
+      version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
