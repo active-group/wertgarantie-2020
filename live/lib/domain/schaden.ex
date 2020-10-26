b6dabb68b87e9f70234a5ee47c72312db1558e36
Tim Digel <tim.digel@active-group.de>
2020-10-26 10:48:57 +0100

    Update

--- live/lib/domain/schaden.ex
+++ live/lib/domain/schaden.ex
@@ -6,12 +6,15 @@ defmodule Live.Domain.Schaden do
   - description (string): Beschreibung des Schadens
   - partner_nr (int): Partner-Nummer der versicherten Person
   """
+  require Logger
+
+  @type partner_nr :: integer()
 
   use QuickStruct,
     id: integer(),
     forecast_amount: float(),
     description: String.t(),
-    partner_nr: integer()
+    partner_nr: partner_nr()
 
   @doc "Bekommt zwei Schäden und liefert den Schaden mit der höheren Schadenssumme zurück"
   @spec with_max_amount(Schaden.t(), Schaden.t()) :: Schaden.t()
@@ -41,5 +44,52 @@ defmodule Live.Domain.Schaden do
   @spec sort_along_amount([Schaden.t()]) :: [Schaden.t()]
   def sort_along_amount(schadensliste) do
     Enum.sort_by(schadensliste, fn schaden -> schaden.forecast_amount end)
+  end
+
+  @doc """
+  Errechnet die Summe einer Liste an Schäden
+  """
+  @spec amount_sum([Schaden.t()]) :: float()
+  # def amount_sum(_schadensliste) do
+  #   0.0
+  # end
+  def amount_sum(schadensliste) do
+    case schadensliste do
+      [] -> 0.0
+      # [el] -> el.forecast_amount
+      # [first, second] -> first.forecast_amount + second.forecast_amount
+      [el | rest] -> el.forecast_amount + amount_sum(rest)
+    end
+  end
+
+  @doc """
+  Überprüft ob alle Schäden einer Liste zu einer übergebenen Partner Nummer gehören
+  """
+  @spec belongs_to_partner?([Schaden.t()], partner_nr()) :: boolean()
+  # def belongs_to_partner?(schadensliste, p_nr) do
+  #   case schadensliste do
+  #     [] -> true
+  #     [el | rest] ->
+  #       el.partner_nr == p_nr and belongs_to_partner?(rest, p_nr)
+  #   end
+  # end
+  def belongs_to_partner?(schadensliste, p_nr) do
+    Enum.all?(schadensliste, fn schaden -> schaden.partner_nr == p_nr end)
+    # Enum.all?(schadensliste, &(&1.partner_nr == p_nr))
+  end
+
+  @doc """
+  Überprüft ob alle Schäden einer Liste zu einer übergebenen Partner Nummer gehören.
+  Die Schadensliste darf nicht leer sein, sonst protokolliere eine Warnmeldung
+  """
+  @spec belongs_to_partner_with_logging?([Schaden.t()], partner_nr()) :: boolean()
+  def belongs_to_partner_with_logging?(schadensliste, p_nr) do
+    if schadensliste == [] do
+      Logger.warn(
+        "Called belongt_to_partner? with empty list and partner_nummer '#{p_nr}'"
+      )
+    end
+
+    belongs_to_partner?(schadensliste, p_nr)
   end
 end
--- live/test/domain/schaden_test.exs
+++ live/test/domain/schaden_test.exs
@@ -4,18 +4,27 @@ defmodule Live.Domain.SchadenTest do
 
   alias Live.Domain.Schaden
 
-  test "schaden with higher amount" do
+  setup_all do
+    # Z. B.
+    # Starte Testdatenbank, gegen die Integrationstest laufen gelassen werden
+
     s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+    s2 = Schaden.make(2, 500, "Stoßstangen sind heutzutage lackiert, da wirds schnell teuer", 9999)
+    s3 = Schaden.make(4, 10, "Stoßstangen sind heutzutage lackiert", 9999)
 
-    s2 =
-      Schaden.make(2, 500, "Stoßstangen sind heutzutage lackiert, da wirds schnell teuer", 9999)
+    # Auch Ausgangstätigkeiten möglich:
+    # on_exit(fn -> IO.puts("Bin fertig") end)
 
-    assert Schaden.with_max_amount(s1, s2) == s2
+    {:ok, s20: s1, s500: s2,  s10: s3}
   end
 
-  test "schaden with equals amount" do
-    s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+  test "schaden with higher amount", %{s20: s20, s500: s_der_gewinnt} do
+    # s1 = %Schaden{id: 1, forecast_amount: 20.0, description: "Bagatelle", partner_nr: 100}
 
+    assert Schaden.with_max_amount(s20, s_der_gewinnt) == s_der_gewinnt
+  end
+
+  test "schaden with equals amount", %{s20: s1} do
     assert Schaden.with_max_amount(s1, %{s1 | description: "Andere Bagatelle"}) == s1
   end
 
@@ -34,5 +43,74 @@ defmodule Live.Domain.SchadenTest do
              ]
 
     assert Schaden.sort_along_amount([]) |> Enum.empty?()
+  end
+
+  describe "Schadenssumme" do
+    test "für die leere Liste" do
+      assert Schaden.amount_sum([]) == 0.0
+    end
+
+    test "für eine Liste mit einem Element" do
+      s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+
+      assert Schaden.amount_sum([s1]) == 20.0
+    end
+
+    test "für eine Liste mit zwei Elementen" do
+      s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+      s2 = Schaden.make(2, 500, "Stoßstangen sind heutzutage lackiert", 9999)
+
+      assert Schaden.amount_sum([s1, s2]) == 520.0
+    end
+
+    test "für eine Liste mit mehr als zwei Elementen" do
+      s1 = Schaden.make(1, 20.0, "Bagatelle", 1001)
+      s2 = Schaden.make(2, 500, "Stoßstangen sind heutzutage lackiert", 9999)
+      s3 = Schaden.make(4, 10, "Stoßstangen sind heutzutage lackiert", 9999)
+
+      assert Schaden.amount_sum([s1, s2, s3]) == 530.0
+    end
+  end
+
+  describe "Gehört zu Partner" do
+    test "bei keinen Schäden" do # was ist der einfachste Fall?
+      assert Schaden.belongs_to_partner?([], 1001)
+    end
+
+    test "bei Schäden die alle dem Partner gehören", %{s500: s500, s10: s10} do
+      assert Schaden.belongs_to_partner?([s500, s10], 9999)
+    end
+
+    test "bei Schäden die alle nicht dem Partner gehören", %{s500: s500, s10: s10} do
+      refute Schaden.belongs_to_partner?([s500, s10], 8888)
+    end
+
+    test "bei Schäden wo einer nicht dem Partner gehört", %{s500: s500, s10: s10, s20: s20} do
+      refute Schaden.belongs_to_partner?([s500, s10, s20], 9999)
+    end
+
+  end
+
+  describe "Gehört zu Partner mit Warnmeldung" do
+    test "bei keinen Schäden" do # was ist der einfachste Fall?
+      assert ExUnit.CaptureLog.capture_log(fn ->
+        assert Schaden.belongs_to_partner_with_logging?([], 1001)
+      end) =~ "Called belongt_to_partner? with empty list and partner_nummer '1001'"
+    end
+
+    test "bei Schäden die alle dem Partner gehören", %{s500: s500, s10: s10} do
+      assert ExUnit.CaptureLog.capture_log(fn ->
+        assert Schaden.belongs_to_partner_with_logging?([s500, s10], 9999)
+      end) == ""
+    end
+
+    test "bei Schäden die alle nicht dem Partner gehören", %{s500: s500, s10: s10} do
+      refute Schaden.belongs_to_partner_with_logging?([s500, s10], 8888)
+    end
+
+    test "bei Schäden wo einer nicht dem Partner gehört", %{s500: s500, s10: s10, s20: s20} do
+      refute Schaden.belongs_to_partner_with_logging?([s500, s10, s20], 9999)
+    end
+
   end
 end
