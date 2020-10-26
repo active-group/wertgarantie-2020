d7e290ca3bf0613e57d929c51d36ab0010aa93de
Tim Digel <tim.digel@active-group.de>
2020-10-26 11:09:30 +0100

    Update

--- live/mix.exs
+++ live/mix.exs
@@ -25,7 +25,8 @@ defmodule Live.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:quick_struct, "~> 0.1"},
-      {:distillery, "~> 2.0"}
+      {:distillery, "~> 2.0"},
+      {:propcheck, "~> 1.1", only: [:dev, :test]}
     ]
   end
 end
